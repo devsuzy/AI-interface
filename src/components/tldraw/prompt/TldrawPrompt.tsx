5e4bafcb8cb5d8bc99cab67b1c1e6a63ef259d3e
안소현 <annsh@imform.co.kr>
2024-08-01 17:32:46 +0900

    캔버스에 데이터 출력하기

--- src/components/tldraw/prompt/TldrawPrompt.tsx
+++ src/components/tldraw/prompt/TldrawPrompt.tsx
@@ -6,12 +6,15 @@ import {
 import { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
 import { track, useEditor } from "tldraw";
 
+type ContentType = { type: string; content: string };
+
 const TldrawPrompt = track(() => {
   const editor = useEditor();
   const { messages } = useChatMessages();
   const { sendMessage, replyMessage, clear } = useChatInteract();
 
   const [value, setValue] = useState("Hi");
+  const [content, setContent] = useState<ContentType[] | undefined>([]);
 
   const handleSendMessage = () => {
     sendMessage({
@@ -36,7 +39,38 @@ const TldrawPrompt = track(() => {
 
   useEffect(() => {
     console.log("change messages", messages, messages.length);
+    const msgContent = messages
+      .filter((msg) => msg.name === "Assistant")
+      .map((msg) =>
+        msg.output
+          .replace(/\[(.*?)\]/g, "$1")
+          .replace(`'type'`, `"type"`)
+          .replace(`'text'`, `"text"`)
+          .replace(`'content'`, `"content"`)
+      )
+      .map((msg) => JSON.parse(JSON.parse(JSON.stringify(msg))));
+
+    setContent(msgContent);
   }, [messages]);
+
+  useEffect(() => {
+    if (content) {
+      content.map((msg) => {
+        editor.createShape({
+          type: "geo",
+          x: 0,
+          y: 0,
+          props: {
+            text: msg.content,
+            color: "blue",
+            w: 400,
+            h: 400,
+            geo: "rectangle",
+          },
+        });
+      });
+    }
+  }, [content]);
 
   return (
     <div className="absolute left-[3rem] top-[6rem] p-[2rem] bg-white">
