6e91c0e883edd8f9d8f2bdbefd837451d004c69a
SRV_PUB_Sangmin1212 <tkdals9048@imform.co.kr>
2024-08-05 18:12:56 +0900

    fix: msw 주석

--- src/main.tsx
+++ src/main.tsx
@@ -8,32 +8,32 @@ import { ChainlitAPI, ChainlitContext } from "@chainlit/react-client";
 const CHAINLIT_SERVER_URL = import.meta.env.VITE_BACKEND_URL;
 const apiClient = new ChainlitAPI(CHAINLIT_SERVER_URL, "webapp");
 
-if (import.meta.env.DEV) {
-  const { worker } = await import("@/mocks/browser");
-  worker.start({
-    onUnhandledRequest(req, print) {
-      const allowedDomain = "chrome-extension:";
-      const allowedDomain2 = "https://cdn.tldraw.com";
-      const allowedDomain3 = "https://fonts.gstatic.com";
-      const allowedDomain4 = CHAINLIT_SERVER_URL;
+// if (import.meta.env.DEV) {
+//   const { worker } = await import("@/mocks/browser");
+//   worker.start({
+//     onUnhandledRequest(req, print) {
+//       const allowedDomain = "chrome-extension:";
+//       const allowedDomain2 = "https://cdn.tldraw.com";
+//       const allowedDomain3 = "https://fonts.gstatic.com";
+//       const allowedDomain4 = CHAINLIT_SERVER_URL;
 
-      if (
-        req.url.pathname.startsWith("/images/") ||
-        req.url.pathname.startsWith("/public/") ||
-        req.url.pathname.startsWith("/src/") ||
-        req.url.pathname.startsWith("/dist/") ||
-        req.url.href.startsWith(allowedDomain) ||
-        req.url.href.startsWith(allowedDomain2) ||
-        req.url.href.startsWith(allowedDomain3) ||
-        req.url.href.startsWith(allowedDomain4)
-      ) {
-        return;
-      }
+//       if (
+//         req.url.pathname.startsWith("/images/") ||
+//         req.url.pathname.startsWith("/public/") ||
+//         req.url.pathname.startsWith("/src/") ||
+//         req.url.pathname.startsWith("/dist/") ||
+//         req.url.href.startsWith(allowedDomain) ||
+//         req.url.href.startsWith(allowedDomain2) ||
+//         req.url.href.startsWith(allowedDomain3) ||
+//         req.url.href.startsWith(allowedDomain4)
+//       ) {
+//         return;
+//       }
 
-      print.warning();
-    },
-  });
-}
+//       print.warning();
+//     },
+//   });
+// }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
